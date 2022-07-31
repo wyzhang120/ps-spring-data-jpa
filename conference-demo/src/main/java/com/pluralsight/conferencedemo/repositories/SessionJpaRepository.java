d21c9387e2a24a28857db40315e1af693256d397
Wenyuan Zhang <wzhang45@slb.com>
2022-07-31 10:07:37 +0800

    added paging and sorting

--- conference-demo/src/main/java/com/pluralsight/conferencedemo/repositories/SessionJpaRepository.java
+++ conference-demo/src/main/java/com/pluralsight/conferencedemo/repositories/SessionJpaRepository.java
@@ -1,7 +1,11 @@
 package com.pluralsight.conferencedemo.repositories;
 
 import com.pluralsight.conferencedemo.models.Session;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -9,4 +13,7 @@ public interface SessionJpaRepository extends JpaRepository<Session, Long> {
     List<Session> findBySessionNameContains(String name);
     List<Session> findBySessionLengthNot(Integer sessionLength);
     List<Session> findBySessionNameNotLike(String name);
+
+    @Query("select s from Session s where s.sessionName like %:name")
+    Page<Session> getSessionsWithName(@Param("name") String name, Pageable pageable);
 }
