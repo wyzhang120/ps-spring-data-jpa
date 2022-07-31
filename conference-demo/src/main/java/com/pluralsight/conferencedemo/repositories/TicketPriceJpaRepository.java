1277538f5b129368f93348993d4639fc817ce283
Wenyuan Zhang <wzhang45@slb.com>
2022-07-31 09:37:57 +0800

    updated custom query with named param

--- conference-demo/src/main/java/com/pluralsight/conferencedemo/repositories/TicketPriceJpaRepository.java
+++ conference-demo/src/main/java/com/pluralsight/conferencedemo/repositories/TicketPriceJpaRepository.java
@@ -3,6 +3,7 @@ package com.pluralsight.conferencedemo.repositories;
 import com.pluralsight.conferencedemo.models.TicketPrice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.math.BigDecimal;
 import java.util.List;
@@ -10,7 +11,7 @@ import java.util.List;
 public interface TicketPriceJpaRepository extends JpaRepository<TicketPrice, Long> {
     @Query("select tp " +
             "from TicketPrice tp " +
-            "where tp.basePrice < ?1 and tp.ticketType.includesWorkshop = true"
+            "where tp.basePrice < :maxPrice and tp.ticketType.includesWorkshop = true"
     )
-    List<TicketPrice> getTicketsUnderPriceWIthWorkshops(BigDecimal maxPrice);
+    List<TicketPrice> getTicketsUnderPriceWIthWorkshops(@Param("maxPrice") BigDecimal maxPrice);
 }
