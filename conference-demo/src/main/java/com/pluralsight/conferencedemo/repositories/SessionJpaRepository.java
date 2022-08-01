449a1ddd2a5a11fcc71fb72e7ab6d3200e7fc607
Wenyuan Zhang <wzhang45@slb.com>
2022-08-01 08:47:54 +0800

    applied excerpt projection

--- conference-demo/src/main/java/com/pluralsight/conferencedemo/repositories/SessionJpaRepository.java
+++ conference-demo/src/main/java/com/pluralsight/conferencedemo/repositories/SessionJpaRepository.java
@@ -1,6 +1,8 @@
 package com.pluralsight.conferencedemo.repositories;
 
 import com.pluralsight.conferencedemo.models.Session;
+import com.pluralsight.conferencedemo.models.projections.SessionDetail;
+import com.pluralsight.conferencedemo.models.projections.SessionDetailView;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,7 +12,10 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
 
-@RepositoryRestResource(path = "conference_sessions", collectionResourceRel = "conference_sessions")
+@RepositoryRestResource(
+        path = "conference_sessions",
+        collectionResourceRel = "conference_sessions",
+        excerptProjection = SessionDetailView.class)
 public interface SessionJpaRepository extends JpaRepository<Session, Long>, SessionCustomJpaRepository {
     List<Session> findBySessionNameContains(String name);
     List<Session> findBySessionLengthNot(Integer sessionLength);
